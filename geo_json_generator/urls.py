a86c9ab972ae5e73410fe552f14cf51f711d0a61
Shubam Mehta <s26mehta@gmail.com>
2023-11-21 18:48:38 -0500

    Fixing issues

--- geo_json_generator/urls.py
+++ geo_json_generator/urls.py
@@ -23,29 +23,23 @@ urlpatterns = [
         name="index",
     ),
     path(
-        "consumers/min_previous_jobs_count=<int:min_previous_jobs_count>/max_previous_jobs_count=<int:max_previous_jobs_count>", 
+        "consumers/min_previous_jobs_count=<int:min_previous_jobs_count>&max_previous_jobs_count=<int:max_previous_jobs_count>", 
         views.index, 
         name="index",
     ),
     path(
-        "consumers/min_previous_jobs_count=<int:min_previous_jobs_count>/max_previous_jobs_count=<int:max_previous_jobs_count>", 
+        "consumers/min_previous_jobs_count=<int:min_previous_jobs_count>&status=<str:status>", 
         views.index, 
         name="index",
     ),
     path(
-        "consumers/min_previous_jobs_count=<int:min_previous_jobs_count>/status=<str:status>", 
+        "consumers/min_previous_jobs_count=<int:min_previous_jobs_count>&max_previous_jobs_count=<int:max_previous_jobs_count>&status=<str:status>", 
         views.index, 
         name="index",
     ),
     path(
-        "consumers/min_previous_jobs_count=<int:min_previous_jobs_count>/max_previous_jobs_count=<int:max_previous_jobs_count>/status=<str:status>", 
+        "consumers/max_previous_jobs_count=<int:max_previous_jobs_count>&status=<str:status>", 
         views.index, 
         name="index",
     ),
 ]
-
-    # path("<int:question_id>/", views.detail, name="detail"),
-    # path("<int:question_id>/results/", views.results, name="results"),
-    # path("<int:question_id>/vote/", views.vote, name="vote"),
-
-    # /consumers?min_previous_jobs_count=2&max_previous_jobs_count=3&status=active
